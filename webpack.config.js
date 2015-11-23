c24a04c0ef798a7329325292d874a327de4b556e
ryym <ryym.64@gmail.com>
2015-11-23 15:31:23 +0900

    Setup configuration target

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var webpack = require('webpack');
+var merge = require('webpack-merge');
 
+const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+// Common configuration.
+var common = {
   entry: PATHS.app,
 
   output: {
@@ -25,27 +28,35 @@ module.exports = {
     ]
   },
 
-  devServer: {
-    historyApiFallback: true,
-    hot: true,
-    // XXX: 理解しきれていないが、これがないとファイルの変更があっても
-    //      ページが自動でリフレッシュされない。
-    inline: true,
-    progress: true,
-
-    // display only errors to reduce the amount of output.
-    stats: 'errors-only',
-
-    // parse host and port from env so this is easy to customize.
-    host: process.env.HOST,
-    port: process.env.PORT
-  },
-
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-
     new HtmlWebpackPlugin({
       title: 'Kanban app'
     })
   ]
 };
+
+if (TARGET === 'start' || ! TARGET) {
+  module.exports = merge(common, {
+    devtool: 'eval-source-map',
+
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+      // XXX: 理解しきれていないが、これがないとファイルの変更があっても
+      //      ページが自動でリフレッシュされない。
+      inline: true,
+      progress: true,
+
+      // display only errors to reduce the amount of output.
+      stats: 'errors-only',
+
+      // parse host and port from env so this is easy to customize.
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+    ]
+  });
+}
