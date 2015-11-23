4e900ca0838e07621c8f1cb8aba4280500d9ab52
ryym <ryym.64@gmail.com>
2015-11-23 12:10:34 +0900

    Plugin: + webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
 
 const PATHS = {
   app: path.join(__dirname, 'app'),
@@ -8,11 +9,31 @@ const PATHS = {
 
 module.exports = {
   entry: PATHS.app,
+
   output: {
     path: PATHS.build,
     filename: 'bundle.js'
   },
+
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    // XXX: 理解しきれていないが、これがないとファイルの変更があっても
+    //      ページが自動でリフレッシュされない。
+    inline: true,
+    progress: true,
+
+    // display only errors to reduce the amount of output.
+    stats: 'errors-only',
+
+    // parse host and port from env so this is easy to customize.
+    host: process.env.HOST,
+    port: process.env.PORT
+  },
+
   plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+
     new HtmlWebpackPlugin({
       title: 'Kanban app'
     })
