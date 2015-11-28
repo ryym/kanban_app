6afaa0198dfafa18110ff48ba479acc7110cb0b0
ryym <ryym.64@gmail.com>
2015-11-28 15:24:07 +0900

    Add a note by 'addNote'

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -23,14 +23,38 @@ export default class App extends React.Component {
         }
       ]
     };
+
+    // NOTE:
+    //  `addNote`が実際に呼び出される時、Reactは`this`を自動で
+    //  この`App`インスタンスにはバインドしてくれない。そのため
+    //  このような処理が必要になる。他の方法として、es7の
+    //  `property-initializer`を先取りして`addNote`というプロパティに
+    //  Arrow Function を持たせておく手もある(es6ではクラスに直接プロパティを
+    //  定義できない)。しかしこの方法は、今だと`constructor()`内での処理に変換される
+    //  っぽい。`constructor()`の変更は Hot-loading で自動更新できず、ページごと
+    //  再読み込みする必要がある。`addNote`の変更もやはり自動更新されないので、ひとまず
+    //  `constructor()`でバインドだけする方法を取る事にする。
+    this.addNote = this.addNote.bind(this);
   }
 
   render() {
     const notes = this.state.notes;
     return (
       <div>
+        <button className="add-note" onClick={this.addNote}>+</button>
         <Notes items={notes} />
       </div>
     );
+  }
+
+  addNote() {
+    this.setState({
+      notes: this.state.notes.concat([
+        {
+          id: uuid.v4(),
+          task: 'New task'
+        }
+      ])
+    });
   }
 }
