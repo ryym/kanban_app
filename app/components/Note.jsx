3598066bae6930fa750d672c77c0415632e64faa
ryym <ryym.64@gmail.com>
2015-11-28 16:08:40 +0900

    Make note editable

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,7 +1,67 @@
 import React from 'react';
 
 export default class Note extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      editing: false
+    }
+
+    this._bindThisContextTo([
+      "renderEdit",
+      "renderTask",
+      "edit",
+      "finishEdit",
+      "checkEnter"
+    ]);
+  }
+
+  _bindThisContextTo(methods) {
+    methods.forEach((method) => {
+      this[method] = this[method].bind(this);
+    })
+  }
+
   render() {
-    return <div>{this.props.task}</div>;
+    const editing = this.state.editing;
+    return (
+      <div>
+        {editing ? this.renderEdit() : this.renderTask()}
+      </div>
+    );
+  }
+
+  renderEdit() {
+    return <input
+      type = "input"
+      autoFocus = {true}
+      defaultValue = {this.props.task}
+      onBlur = {this.finishEdit}
+      onKeyPress = {this.checkEnter}
+    />;
+  }
+
+  renderTask() {
+    return <div onClick={this.edit}>{this.props.task}</div>;
+  }
+
+  edit() {
+    this.setState({
+      editing: true
+    });
+  }
+
+  checkEnter(e) {
+    if(e.key === 'Enter') {
+      this.finishEdit(e);
+    }
+  }
+
+  finishEdit(e) {
+    this.props.onEdit(e.target.value);
+    this.setState({
+      editing: false
+    });
   }
 }
