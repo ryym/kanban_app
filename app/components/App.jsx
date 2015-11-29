a57d5c92ec65de620fcd5d46c089247e28a1c22f
ryym <ryym.64@gmail.com>
2015-11-29 15:49:50 +0900

    Use Alt container
    which inject updated stores to child elements

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 import Notes from './Notes.jsx';
 import NoteActions from '../actions/NoteActions';
 import NoteStore from '../stores/NoteStore';
+import AltContainer from 'alt-container';
 import * as u from '../util.js';
 
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = NoteStore.getState();
 
     // NOTE:
     //  `addNote`が実際に呼び出される時、Reactは`this`を自動で
@@ -24,29 +24,22 @@ export default class App extends React.Component {
     u.bindMethodContexts(this, [
       'addNote',
       'editNote',
-      'deleteNote',
-      'storeChanged'
+      'deleteNote'
     ]);
   }
 
-  componentDidMount() {
-    NoteStore.listen(this.storeChanged);
-  }
-
-  componentWillUnmount() {
-    NoteStore.unlisten(this.storeChanged);
-  }
-
-  storeChanged(state) {
-    this.setState(state);
-  }
-
   render() {
-    const notes = this.state.notes;
     return (
       <div>
         <button className="add-note" onClick={this.addNote}>+</button>
-        <Notes items={notes} onEdit={this.editNote} onDelete={this.deleteNote} />
+        <AltContainer
+          stores={[NoteStore]}
+          inject={{
+            items: () => NoteStore.getState().notes
+          }}
+        >
+          <Notes onEdit={this.editNote} onDelete={this.deleteNote} />
+        </AltContainer>
       </div>
     );
   }
