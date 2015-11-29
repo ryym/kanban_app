a65df55833ed437485a58de99d157675656f332e
ryym <ryym.64@gmail.com>
2015-11-29 16:36:03 +0900

    Replace Notes with Lanes in App

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 import Notes from './Notes.jsx';
 import NoteActions from '../actions/NoteActions';
 import NoteStore from '../stores/NoteStore';
+import Lanes from './Lanes.jsx';
+import LaneActions from '../actions/LaneActions';
+import LaneStore from '../stores/LaneStore';
 import AltContainer from 'alt-container';
 import * as u from '../util.js';
 
@@ -22,37 +25,27 @@ export default class App extends React.Component {
     //  再読み込みする必要がある。`addNote`の変更もやはり自動更新されないので、ひとまず
     //  `constructor()`でバインドだけする方法を取る事にする。
     u.bindMethodContexts(this, [
-      'addNote',
-      'editNote',
-      'deleteNote'
+      'addItem',
     ]);
   }
 
   render() {
     return (
       <div>
-        <button className="add-note" onClick={this.addNote}>+</button>
+        <button className="add-lane" onClick={this.addItem}>+</button>
         <AltContainer
-          stores={[NoteStore]}
+          stores={[LaneStore]}
           inject={{
-            items: () => NoteStore.getState().notes
+            items: () => LaneStore.getState().lanes
           }}
         >
-          <Notes onEdit={this.editNote} onDelete={this.deleteNote} />
+          <Lanes />
         </AltContainer>
       </div>
     );
   }
 
-  addNote() {
-    NoteActions.create({task: 'New Task'});
-  }
-
-  editNote(id, task) {
-    NoteActions.update({id, task});
-  }
-
-  deleteNote(id) {
-    NoteActions.delete(id);
+  addItem() {
+    LaneActions.create({name: 'New Lane'});
   }
 }
