9f9ac467a17ad6ffeda402b416cccbeb58da4a5b
ryym <ryym.64@gmail.com>
2015-11-29 11:39:33 +0900

    Implement Alt components

---
+++ app/libs/alt.js
@@ -0,0 +1,6 @@
+import Alt from 'alt';
+
+// Use Alt as a singleton.
+const alt = new Alt();
+
+export default alt;
---
+++ app/stores/NoteStore.js
@@ -0,0 +1,49 @@
+import uuid from 'node-uuid';
+import alt from '../libs/alt';
+import NoteActions from '../actions/NoteActions';
+
+class NoteStore {
+  constructor() {
+    this.bindActions(NoteActions);
+    this.notes = [];
+  }
+
+  create(note) {
+    const notes = this.notes;
+    note.id = uuid.v4();
+    this.setState({
+      notes: notes.concat(note)
+    });
+  }
+
+  update({id, task}) {
+    const notes = this.notes;
+    const idx = this.findNoteIdx(id);
+    if (idx < 0) {
+      return;
+    }
+    notes[idx].task = task;
+     // Optional(Manipulating `this.notes` directly also work.)
+    this.setState({notes});
+  }
+
+  delete(id) {
+    const notes = this.notes;
+    const idx = this.findNoteIdx(id);
+    notes.splice(idx, 1);
+    this.setState({
+      notes: notes
+    });
+  }
+
+  findNoteIdx(id) {
+    const notes = this.notes;
+    const idx = notes.findIndex((note) => note.id === id);
+    if (idx < 0) {
+      console.warn('Failed to find note', notes, id);
+    }
+    return idx;
+  }
+}
+
+export default alt.createStore(NoteStore, 'NoteStore');
