7ec85439ec1311902f061981587b51a8e616c71d
ryym <ryym.64@gmail.com>
2015-12-06 21:43:11 +0900

    Add tests for Note component

---
+++ tests/note_test.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import TestBackend from 'react-dnd-test-backend';
+import {DragDropContext} from 'react-dnd';
+import assert from 'assert';
+import Note from 'app/components/Note.jsx';
+
+describe('Note', () => {
+  // https://gaearon.github.io/react-dnd/docs-testing.html
+  function wrapTestContext(DecoratedComponent) {
+    @DragDropContext(TestBackend)
+    class TestContextContainer extends React.Component {
+      render() {
+        return <DecoratedComponent {...this.props} />;
+      }
+    }
+    return TestContextContainer;
+  }
+
+  function findListDOM(component) {
+    return TestUtils.findRenderedDOMComponentWithTag(component, 'li');
+  }
+
+  it('renders children', () => {
+    const children = 'test';
+    const NoteContent = wrapTestContext(Note);
+    const root = TestUtils.renderIntoDocument(
+      <NoteContent>{children}</NoteContent>
+    );
+
+    assert.equal(root.props.children, children);
+  });
+
+  it('become transparent while dragging', () => {
+    const NoteContent = wrapTestContext(Note);
+    const root = TestUtils.renderIntoDocument(
+      <NoteContent />
+    );
+    const backend = root.getManager().getBackend();
+
+    assert.equal(findListDOM(root).style.opacity, 1);
+
+    const note = TestUtils.findRenderedComponentWithType(root, Note);
+    backend.simulateBeginDrag([ note.getHandlerId() ]);
+
+    assert.equal(findListDOM(root).style.opacity, 0);
+  });
+
+});
